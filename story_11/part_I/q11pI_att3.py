da800243a40fe555768638a33fd292a3f1d8528a
MusangQim <musangqim03@gmail.com>
2025-11-18 18:43:12 +0800

    add try n error file

---
+++ story_11/part_I/q11pI_att3.py
@@ -0,0 +1,35 @@
+#####################################
+#  ___                  _       _ _ #
+# / _ \ _   _  ___  ___| |_    / / |#
+#| | | | | | |/ _ \/ __| __|   | | |#
+#| |_| | |_| |  __/\__ \ |_    | | |#
+# \__\_\\__,_|\___||___/\__|___|_|_|#
+#        |_____|   #
+#####################################
+# ---------------PART I-------------------------------------
+# cols = [9, 1, 1, 4, 9, 6]
+duck = [2, 1, 17, 12, 19, 9]
+
+phase = 1
+lap = 10
+while lap > 0:
+    change = False
+    for i in range(len(duck) - 1):
+        if phase == 1:
+            if duck[i] > duck[i + 1]:
+                duck[i] -= 1
+                duck[i + 1] += 1
+                change = True
+        else:
+            if duck[i] < duck[i + 1]:
+                duck[i] += 1
+                duck[i + 1] -= 1
+                change = True
+    if change:
+        lap -= 1
+    else:
+        if phase == 1:
+            phase = 2
+        else:
+            break
+    print(sum(i * x for i, x in enumerate(duck, 1)))
